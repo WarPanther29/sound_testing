a0ccb7706c8b928f310928edb09cb880fa7c98f9
Felix <felix.matzdorf@stud.tu-darmstadt.de>
2025-08-04 17:34:59 +0200

    update client

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 import time
 import sys
 import json
+import client
 from smbus2 import SMBus, i2c_msg
 import paho.mqtt.client as mqtt
+import socket
 
 # MQTT
 MQTT_BROKER = "192.168.178.56"
 MQTT_PORT = 1883
-MQTT_TOPIC = "microphones/values"
+#MQTT_TOPIC = "robots/+/mics"
 
 # e-puck2
 I2C_CHANNEL = 12
@@ -16,7 +18,6 @@ ROB_ADDR = 0x1F
 ACTUATORS_SIZE = 20
 SENSORS_SIZE = 47
 
-# Initialize I2C
 try:
     bus = SMBus(I2C_CHANNEL)
 except:
@@ -41,21 +42,18 @@ def update_robot():
     except:
         return False
 
-# Verify checksum
 def verify_checksum():
     checksum = 0
     for i in range(SENSORS_SIZE - 1):
         checksum ^= sensors_data[i]
     return checksum == sensors_data[SENSORS_SIZE - 1]
 
-# Read microphone data
 def read_mic_values():
     mic = [0] * 4
     for i in range(4):
         mic[i] = sensors_data[32 + i*2 + 1] * 256 + sensors_data[32 + i*2]
     return mic
 
-# Collect 100 samples per mic channel
 def collect_mic_samples(sample_count=400):
     mic_data = [[] for _ in range(4)]
 
@@ -78,19 +76,22 @@ def collect_mic_samples(sample_count=400):
     print("Mic samples collected in {:.2f} seconds.".format(time.time() - start))
     return mic_data
 
-# Main MQTT publisher
+
 def main():
     client = mqtt.Client()
     client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    hostname = socket.gethostname()
+    number = ''.join(filter(str.isdigit, hostname[len('pi-puck'):]))
 
-    
-    for _ in range(10):
+    MQTT_TOPIC = f"robots/{number}/mics"
+
+    while True:
         mic_data = collect_mic_samples()
         payload = json.dumps({
-            "mic_0": mic_data[0],
-            "mic_1": mic_data[1],
-            "mic_2": mic_data[2],
-            "mic_3": mic_data[3]
+            "m0": mic_data[0],
+            "m1": mic_data[1],
+            "m2": mic_data[2],
+            "m3": mic_data[3]
         })
 
         result = client.publish(MQTT_TOPIC, payload)
